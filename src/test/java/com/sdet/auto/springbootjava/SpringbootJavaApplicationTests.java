3a121bb07ea323c999736082a0f3a5873485190e
sdetAutomation <sdet.testautomation@gmail.com>
2018-05-18 17:02:15 -0500

    init commit

---
+++ src/test/java/com/sdet/auto/springbootjava/SpringbootJavaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sdet.auto.springbootjava;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootJavaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
